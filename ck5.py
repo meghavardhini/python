c02888700845a6f3f814b2ead5267eda0bda7437
meghavardhini <53110702+meghavardhini@users.noreply.github.com>
2019-07-21 19:38:47 +0530

    Add files via upload

---
+++ ck5.py
@@ -0,0 +1,12 @@
+def mn():
+    a=int(input("enter the numers:"))
+    b=int(input("enter the numers:"))
+    c=int(input("enter the numers:"))
+    if(a>b and a>c):
+        print("a is the greatest")
+    elif(b>c and b>a):
+        print("b is the greatest")
+    else:
+        print("c is the greatest")
+mn()
+        
