3a872f1f64ae0910aab472a6ee639ff6e38519bc
meghavardhini <53110702+meghavardhini@users.noreply.github.com>
2019-07-21 18:33:00 +0530

    Add files via upload

---
+++ ck1.py
@@ -0,0 +1,9 @@
+def mn():
+    a=int(input("enter the value of a"))
+    if(a>=3):
+        print("it is a positive num")
+    elif(a<3):
+        print("itis a negative num")
+    elif(a==0):
+     print("it is zero")
+mn()
