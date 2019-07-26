95a43bbcf6d1d7d5f4cb3e4ed9709130168b837a
meghavardhini <53110702+meghavardhini@users.noreply.github.com>
2019-07-26 20:00:30 +0530

    Add files via upload

---
+++ ck24.py
@@ -0,0 +1,9 @@
+str1 = input("Please Enter your Own String : ").split()
+total = 0
+i = 0
+
+while(i < len(str1)):
+    total = total + 1
+    i = i + 1
+ 
+print("Total Number of Characters in this String = ", total)
