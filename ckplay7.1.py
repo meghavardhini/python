e9cd6a4264a7878af2ce1ed08a949beb43cf4ab6
meghavardhini <53110702+meghavardhini@users.noreply.github.com>
2019-08-12 20:48:31 +0530

    Add files via upload

---
+++ ckplay7.1.py
@@ -0,0 +1,7 @@
+list1=['I','II','III','IV','V','VI','VII','VIII','IX','X','XI','XII','XIII','XIV','XV','XVI','XVII','XVIII','XXI','XX']
+list2=[1,2,3,4,5,6,7,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+m=str(input())
+for i in range(7):
+    if(m==list1[i]):
+       print(list2[i])
+       break
