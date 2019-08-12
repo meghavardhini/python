9ad86a5203d1a576025a0c9a263e313d268bc88d
meghavardhini <53110702+meghavardhini@users.noreply.github.com>
2019-08-12 09:28:17 +0530

    Add files via upload

---
+++ ckpro13.py
@@ -0,0 +1,11 @@
+m,n=map(int,input().split())
+o=list(map(int,input().split()))
+l1=[]
+for i in range(0,n):
+     a,b=map(int,input().split())
+     l1.append([a,b])
+for i in range(n):
+     lower=(l1[i][0])
+     upper=(l1[i][1])
+     x=min(o[lower-1:upper])
+     print(x)
